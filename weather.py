fc14317d689f1e3d72a66142f6d1c4bf5d6a34b7
hp <lopuabrenda63@gmail.com>
2026-04-08 16:56:18 +0300

    Formating and displaying a nice weather report

--- weather.py
+++ weather.py
@@ -15,6 +15,22 @@ temp = data["main"] ["temp"]
 humidity = data ["main"] ["humidity"]
 description = data["weather"][0]["description"]
 
-print(temp)
-print(humidity)
-print(description)
+if "clear" in description.lower():
+   emoji ="☀️"
+   
+elif "cloud" in description.lower():
+    emoji ="⛅"
+    
+elif "rainy" in description.lower():
+    emoji="🌧️"
+    
+else:
+    emoji="✨"
+    
+# print(temp)
+# print(humidity)
+# print(description)
+
+print(f"weather in Nairobi:{description}{emoji}")
+print(f"Temperature:{temp}°C")
+print(f"Humidity:{humidity}%")
